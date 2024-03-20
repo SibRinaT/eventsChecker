4d621a1dd470ff4f26d110168528c9b9817bc1ea
Ainur <sibrinat616@gmail.com>
2024-03-20 19:39:31 +0300

    git init

---
+++ eventProject/eventProject/eventProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventProjectApp.swift
+//  eventProject
+//
+//  Created by Ainur on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct eventProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
