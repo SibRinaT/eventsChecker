56c943c6fcf17d27f74f5ee6651cbb1e8ab0de1c
Ainur <sibrinat616@gmail.com>
2024-03-22 20:25:45 +0300

    created helpView for open another views from HomeView

---
+++ eventProject/eventProject/HelpViews/OtherWindowView.swift
@@ -0,0 +1,41 @@
+//
+//  OtherWindowView.swift
+//  eventProject
+//
+//  Created by Ainur on 22.03.2024.
+//
+
+import SwiftUI
+
+struct OtherWindowView: View {
+    @State var icon: String
+    @State var title: String
+    @State var text: String
+
+    var body: some View {
+        VStack {
+            Rectangle()
+                .frame(width: 169, height: 169)
+                .foregroundColor(Color(.gray))
+                .cornerRadius(15)
+                .overlay (
+                    VStack(alignment:.leading){
+                        Image(icon)
+                            .foregroundColor(.white)
+                        Text(title)
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(.white)
+                        Text(text)
+                            .font(.caption)
+                            .foregroundColor(.white)
+                        
+                    }
+                )
+        }
+    }
+}
+
+#Preview {
+    OtherWindowView(icon: "", title: "", text: "")
+}
