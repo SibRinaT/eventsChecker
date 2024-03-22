93a7b10e81f84555dbbf35d7c4688ca9ec7e8720
Ainur <sibrinat616@gmail.com>
2024-03-22 20:53:24 +0300

    added helpView in HomeView

---
+++ eventProject/eventProject/HelpViews/testView.swift
@@ -0,0 +1,18 @@
+//
+//  testView.swift
+//  eventProject
+//
+//  Created by Ainur on 22.03.2024.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testView()
+}
