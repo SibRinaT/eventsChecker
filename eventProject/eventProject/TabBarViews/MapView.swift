8f58db2a6e66b4117af28a0a1264eef2e24bf853
Ainur <sibrinat616@gmail.com>
2024-03-25 16:39:16 +0300

    added alert, added description for city (but this not right design)

--- eventProject/eventProject/TabBarViews/MapView.swift
+++ eventProject/eventProject/TabBarViews/MapView.swift
@@ -14,12 +14,49 @@ struct MapView: View {
           span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
       )
       
+    @State private var showingLocationAlert = false
+
       var body: some View {
-          Map(coordinateRegion: $region)
-              .frame(height: 300)
-          
-          
+          VStack {
+              Map(coordinateRegion: $region)
+                  .frame(height: 300)
+              HStack {
+                  Text("Ваше местоположение,")
+                      .bold()
+                      .font(.headline)
+                              Text("Изменить")
+                                  .onTapGesture {
+                                      self.showingLocationAlert = true
+                                  }
+                                  .padding()
+                          }
+              .alert(isPresented: $showingLocationAlert) {
+                  Alert(
+                    title: 
+                        Text("Разрешение на использование геолокации")
+                        .foregroundColor(Color("orangeColor")),
+                    message: Text("Предоставить доступ к вашему местоположению?"),
+                    primaryButton: .default(Text("Да")) {
+//
+                    },
+                    secondaryButton: .cancel(Text("Отмена"))
+                  )
+              }
+              Spacer()
+              VStack {
+                  Text("Что за город?")
+                      .font(.title)
+                      .multilineTextAlignment(.leading)
+                  
+                  Text("Описание города Альметьевск: Альме́тьевск — город в Республике Татарстан Российской Федерации. Административный центр Альметьевского района. Образует городское поселение город Альметьевск. Самый крупный город в полицентрической Альметьевско-Бугульминско-Лениногорской агломерации и четвёртый по численности населения город Татарстана, центр Юго-Восточной экономической зоны Татарстана. «Велостолица» России")
+                      .multilineTextAlignment(.center)
+              }
+              Spacer()
+          }
+          .background(Color("whiteColor"))
+//          .edgesIgnoringSafeArea(.all)
       }
+    
 }
 
 #Preview {
