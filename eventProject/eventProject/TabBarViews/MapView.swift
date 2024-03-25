f1c1d49fa23c8afd971c9a9ac6c6b8a0b6e3efaa
Ainur <sibrinat616@gmail.com>
2024-03-25 16:22:29 +0300

    map init

--- eventProject/eventProject/TabBarViews/MapView.swift
+++ eventProject/eventProject/TabBarViews/MapView.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    @State private var region = MKCoordinateRegion(
+          center: CLLocationCoordinate2D(latitude: 54.9044, longitude: 52.3154),
+          span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+      )
+      
+      var body: some View {
+          Map(coordinateRegion: $region)
+              .frame(height: 300)
+          
+          
+      }
 }
 
 #Preview {
